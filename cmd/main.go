0c613f97c3d646dcadf9c95e1599e4411a440d20
steve <snguyen@valleyit.com>
2025-10-15 22:16:28 -0500

    Add webhook support for CronJob; update Kustomization and RBAC roles for webhook management; enhance e2e tests for cert-manager and CA injection validation.

--- cmd/main.go
+++ cmd/main.go
@@ -17,28 +17,27 @@ limitations under the License.
 package main
 
 import (
-    "crypto/tls"
-    "flag"
-    "os"
+	"crypto/tls"
+	"flag"
+	"os"
 
-    // Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-    // to ensure that exec-entrypoint and run can make use of them.
-    _ "k8s.io/client-go/plugin/pkg/client/auth"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
-    "k8s.io/apimachinery/pkg/runtime"
-    utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-    clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/healthz"
-    "sigs.k8s.io/controller-runtime/pkg/log/zap"
-    "sigs.k8s.io/controller-runtime/pkg/metrics/filters"
-    metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-    "sigs.k8s.io/controller-runtime/pkg/webhook"
-
-    // webhookv1 "xilia.net/guestbook/internal/webhook/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	webappv1 "xilia.net/guestbook/api/v1"
 	"xilia.net/guestbook/internal/controller"
+	webhookv1 "xilia.net/guestbook/internal/webhook/v1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -195,14 +194,13 @@ func main() {
 		os.Exit(1)
 	}
 
-    // nolint:goconst
-    // if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-    //     if err := webhookv1.SetupCronJobWebhookWithManager(mgr); err != nil {
-    //         setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
-    //         os.Exit(1)
-    //     }
-    // }
-
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := webhookv1.SetupCronJobWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
+			os.Exit(1)
+		}
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
